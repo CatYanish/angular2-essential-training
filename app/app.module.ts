466fd7967869689f29335f7bd6c69fc31c4c0b4b
Cat Yanish <catyanish@gmail.com>
2017-11-03 21:45:18 -0500

    working in app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,1 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 
-@NgModule({
-  import: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
-})
-export class AppModule {}
